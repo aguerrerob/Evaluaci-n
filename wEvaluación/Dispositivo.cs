e40e83a58bf281140034e40942e1a36b1badc80c
aguerrerob <60979875+aguerrerob@users.noreply.github.com>
2025-10-14 15:58:08 -0500

    Add files via upload

---
+++ wEvaluación/Dispositivo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// TODO: Analizar por qué el polimorfismo no funciona como se espera.
+
+namespace wEvaluación
+{
+    public class Dispositivo
+    {
+        public string Nombre { get; set; }
+        public double CostoBase { get; set; }
+
+        public virtual double CalcularMantenimiento()
+        {
+            return CostoBase * 0.10;
+        }
+    }
+
+    public class Laptop : Dispositivo
+    {
+        
+        public new double CalcularMantenimiento()
+        {
+            return CostoBase * 0.15;
+        }
+    }
+
+    public class Tablet : Dispositivo
+    {
+        
+        public double CalcularMantenimiento(double extra)
+        {
+            return (CostoBase * 0.05) + extra;
+        }
+    }
+
+    public class Celular : Dispositivo
+    {
+        
+        public override double CalcularMantenimiento()
+        {
+            return 100000;
+        }
+    }
+}
